bba5e65c6518539e23c6eeb0fa4d1c9883a2676e
Paloma Rizzon <paloma.rizzon@sga.pucminas.br>
2023-10-08 12:38:28 -0300

    Update

---
+++ mf-dev-backend-2023/Controllers/VeiculosController.cs
@@ -0,0 +1,22 @@
+﻿using mf_dev_backend_2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2023.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+            
+            return View(dados);
+        }
+    }
+}
--- mf-dev-backend-2023/Models/Veiculo.cs
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_backend_2023.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação do veículo")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano do veículo")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
